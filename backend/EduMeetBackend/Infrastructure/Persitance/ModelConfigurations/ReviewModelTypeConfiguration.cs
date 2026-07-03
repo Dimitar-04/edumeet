950c1c1ce28c2453931aedba77cc726cf661bb40
Dimitar Arsov <dimitararsov04@gmail.com>
2026-07-03 22:26:00 +0200

    add models, dbcontext and migrations

---
+++ backend/EduMeetBackend/Infrastructure/Persitance/ModelConfigurations/ReviewModelTypeConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using _1._Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace _3._Infrastracture.Persitance.ModelConfigurations;
+
+public class ReviewModelTypeConfiguration:IEntityTypeConfiguration<Review>
+{
+    public void Configure(EntityTypeBuilder<Review> builder)
+    {
+        builder.HasKey(r => r.Id);
+        builder.HasKey(r => r.Id);
+
+        builder.Property(r => r.Grade)
+            .IsRequired();
+
+        builder.Property(r => r.Description)
+            .HasMaxLength(1000);
+
+        builder.HasOne(r => r.EducationalEvent)
+            .WithMany(e => e.Reviews)
+            .HasForeignKey(r => r.EducationalEventId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.ToTable(t =>
+        {
+            t.HasCheckConstraint(
+                "CK_Review_Grade",
+                "\"Grade\" >= 1 AND \"Grade\" <= 5");
+        });
+
+        builder.HasIndex(r => r.EducationalEventId);
+    }
+}
